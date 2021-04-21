9b138f6361865f235ad0c701a97744d842621f6f
Sean Xiao <jean.paul.1685@outlook.com>
2021-04-21 12:55:47 -0400

    Add theming

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAB8u6G-HvrOO7ukfP9tHVejFF26WU6kNY',
+    authDomain: 'angular1-747a1.firebaseapp.com',
+    databaseURL: 'https://angular1-747a1-default-rtdb.firebaseio.com',
+    projectId: 'angular1-747a1',
+    storageBucket: 'angular1-747a1.appspot.com',
+    messagingSenderId: '705700989133',
+    appId: '1:705700989133:web:f236d71d09e5ad3e158184',
+  },
 };
